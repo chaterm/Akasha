e32ecc08ee185b6373e8c0cb23c0b094a1c8ee96
fishyuemail <fishyuemail@163.com>
2026-06-09 18:00:06 +0800

    feat(api-key): register ApiKeyModule in EeModule

--- apps/server/src/ee/ee.module.ts
+++ apps/server/src/ee/ee.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { SsoModule } from './sso/sso.module';
+import { ApiKeyModule } from './api-key/api-key.module';
 
 @Module({
-  imports: [SsoModule],
+  imports: [SsoModule, ApiKeyModule],
 })
 export class EeModule {}
